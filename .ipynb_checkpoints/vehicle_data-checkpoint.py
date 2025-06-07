6f19b272cd6f8abaceac6c0d367d75c721c91259
KDitsa <kditsa2023@gmail.com>
2025-06-07 16:51:40 +0530

    Add .gitignore and integrate timescaleDB

---
+++ .ipynb_checkpoints/vehicle_data-checkpoint.py
@@ -0,0 +1,14 @@
+import random
+from datetime import datetime, timezone
+
+# create random information about vehicles
+def generate_vehicle_data(v_id):
+    return {
+        'vehicle_id': v_id,
+        'lat': round(random.uniform(-90,90),6),
+        'long': round(random.uniform(-180,180),6),
+        'speed': round(random.uniform(0,200),2),
+        'temperature': round(random.uniform(50,150),2),
+        'humidity': round(random.uniform(10,90),2),
+        'timestamp': datetime.now(timezone.utc).isoformat()
+    }
